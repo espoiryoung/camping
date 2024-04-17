42d5da7e9c8dd77a2fe07c02463dcfe83cecbd12
전진영 <espoiryoung@naver.com>
2024-04-17 16:04:30 +0900

    Header 컴포넌트 분리

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+import { View,TouchableOpacity, Image, Text, StyleSheet } from "react-native"
+
+type Props = {
+    title:string;
+}
+
+const Header=({title}:Props)=>{
+
+    return (
+        <View style={styles.headerArea}> 
+            <TouchableOpacity >
+                <Image source={require('@icons/leftArrow.png')}/>
+            </TouchableOpacity>
+            <Text style={{ color:"#573353",fontSize:18, fontWeight:'bold', backgroundColor:"#000"}}>{title}</Text>
+        </View>
+    )
+
+}
+
+export default Header;
+
+const styles = StyleSheet.create({
+    headerArea:{
+        
+        backgroundColor:"#FFF3E9",
+        paddingHorizontal:15,
+        alignItems:'center',
+        flexDirection:'row'
+
+    },
+
+})
